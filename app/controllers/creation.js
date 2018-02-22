fa5ad059c2c7e2c82bf8abb6a40eb0a2ad6be82c
xieke <xiekexk@hfbank.com.cn>
2018-02-22 16:39:08 +0800

    .

---
+++ app/controllers/creation.js
@@ -0,0 +1,50 @@
+'use strict'
+var mongoose=require('mongoose')
+var User=mongoose.model('User')
+var Video=mongoose.model('Video')
+// var sha1=require('sha1')
+var config=require('../../config/config')
+var robot=require('../service/robot')
+// var uuid=require('uuid')
+
+exports.video=function* (next) {
+	var body=this.request.body
+	var videoData=body.video
+	var user=this.session.user
+
+	if (!videoData||!videoData.qiniu_key) {
+		this.body={
+			success:false,
+			err:'视频没有上传成功'
+		}
+		return next
+	};
+
+	var video=yield Video.findOne({
+		qiniu_key:videoData.qiniu_key
+	}).exec()
+
+	if (!video) {
+		// 如果没有视频记录的话，则先保存视频
+		video=new Video({
+			author:user._id,
+			qiniu_key:videoData.qiniu_key,
+			persistentId:videoData.persistentId
+		})
+		video=yield video.save()
+	};
+
+	// 需要将该视频的静音文件异步上传到cloudinary平台
+	// var url=config.qiniu.video+video.qiniu_key
+
+	this.body={
+		//将视频的id即视频的地址返还给前端
+		success:true,
+		data:video._id
+	}
+
+
+}
+
+
+
--- config/routes.js
+++ config/routes.js
@@ -2,6 +2,7 @@
 var Router= require('koa-router')
 var User=require('../app/controllers/user')
 var App=require('../app/controllers/app')
+var Creation=require('../app/controllers/creations')
 module.exports=function(){
   var router = new Router({
     prefix:'/api'
@@ -10,5 +11,7 @@ module.exports=function(){
   router.post('/u/verify',App.hasBody,User.verify)
   router.post('/u/update',App.hasBody,App.hasToken,User.update)
   router.post('/signature',App.hasBody,App.hasToken,App.signature)
+
+  router.post('/creations/video',App.hasBody,App.hasToken,Creation.video)
   return router
 }
