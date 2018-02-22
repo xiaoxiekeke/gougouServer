63b4ef210da00cd519f2f91a82a65299196cf284
xieke <xiekexk@hfbank.com.cn>
2018-02-22 17:49:29 +0800

    .

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -158,60 +158,124 @@ var uuid = require('uuid')
 var sms = require('../service/sms')
 
 
-exports.signup = function *(next) {
-  var phoneNumber = xss(this.request.body.phoneNumber.trim())
+// exports.signup = function *(next) {
+//   var phoneNumber = xss(this.request.body.phoneNumber.trim())
 
+//   var user = yield User.findOne({
+//     phoneNumber: phoneNumber
+//   }).exec()
+
+//   var verifyCode = sms.getCode()
+
+//   if (!user) {
+//     var accessToken = uuid.v4()
+
+//     user = new User({
+//       nickname: '小狗宝',
+//       avatar: 'http://res.cloudinary.com/gougou/image/upload/mooc1.png',
+//       phoneNumber: xss(phoneNumber),
+//       verifyCode: verifyCode,
+//       accessToken: accessToken
+//     })
+//   }
+//   else {
+//     user.verifyCode = verifyCode
+//   }
+
+//   try {
+//     user = yield user.save()
+//   }
+//   catch (e) {
+//     this.body = {
+//       success: false
+//     }
+
+//     return next
+//   }
+
+//   var msg = '您的注册验证码是：' + user.verifyCode
+
+//   try {
+//     sms.send(user.phoneNumber, msg)
+//   }
+//   catch (e) {
+//     console.log(e)
+
+//     this.body = {
+//       success: false,
+//       err: '短信服务异常'
+//     }
+
+//     return next
+//   }
+
+//   this.body = {
+//     success: true
+//   }
+// }
+exports.signup=function *(next) {
+
+  //从数据库中查找当前phoneNumber的用户
+  var phoneNumber=xss(this.request.body.phoneNumber.trim())
+
+  if(!phoneNumber){
+    this.body={
+      success:false,
+      err:'请输入手机号'
+    }
+    return next
+  }
+
+  // var phoneNumber=this.query.phoneNumber
   var user = yield User.findOne({
-    phoneNumber: phoneNumber
+    phoneNumber:phoneNumber
   }).exec()
 
-  var verifyCode = sms.getCode()
 
+  var verifyCode=sms.getCode()
+
+  //如果用户不存在则创建新用户,若存在，则设置verifyCode
   if (!user) {
-    var accessToken = uuid.v4()
-
+    var accessToken=uuid.v4()
+    //创建Entity：由Model创建的实体，使用save方法保存数据
     user = new User({
-      nickname: '小狗宝',
-      avatar: 'http://res.cloudinary.com/gougou/image/upload/mooc1.png',
-      phoneNumber: xss(phoneNumber),
-      verifyCode: verifyCode,
-      accessToken: accessToken
+      nickname:'小狗宝',
+      phoneNumber:xss(phoneNumber),
+      avatar:'http://res.cloudinary.com/xiaoke/image/upload/v1516267703/avatar/s3mjsfkzulpubzc0sk0k.jpg',
+      verifyCode:verifyCode,
+      accessToken:accessToken,
     })
-  }
-  else {
+  }else {
     user.verifyCode = verifyCode
   }
 
   try {
-    user = yield user.save()
-  }
-  catch (e) {
-    this.body = {
-      success: false
+    user=yield user.save()
+    //使用Entity的save方法保存数据
+  } catch (e) {
+    this.body={
+      success:false
     }
-
     return next
   }
+  
+  var msg='您的注册验证码是：'+user.verifyCode
+  // try{
+  //   sms.send(user.phoneNumber,msg)    
+  // }
+  // catch(e){
+  //   this.body={
+  //     success:false,
+  //     err:'短信服务异常'
+  //   }
+  //   return next
+  // }
 
-  var msg = '您的注册验证码是：' + user.verifyCode
-
-  try {
-    sms.send(user.phoneNumber, msg)
+  this.body={
+    success:true,
+    msg:msg
   }
-  catch (e) {
-    console.log(e)
 
-    this.body = {
-      success: false,
-      err: '短信服务异常'
-    }
-
-    return next
-  }
-
-  this.body = {
-    success: true
-  }
 }
 
 exports.verify = function *(next) {
